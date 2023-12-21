db7237d7c8f55facea38a74509c9c56023247d0e
beni-f <benifissha@gmail.com>
2023-12-21 22:27:29 +0300

    initial commit

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,20 +115,31 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        try:
+            if not args:
+                raise SyntaxError()
+            list_args = args.split("=")
+            new_instance = eval("{}()".format(list_args[0]))
+            for arg in list_args:
+                key, value = arg.split("=")
+                value = eval(value)
+                if type(value) not in {int, float, str}:
+                    continue
+                if type(value) is str:
+                    value.replace("_", " ").replace('"', '\\"')
+                new_instance[key] = value
+            new_instance.save()
+            print("{}".format(new_instance.id))
+            new_instance.save()
+        except SyntaxError:
+            print("** class is missing **")
+        except NameError:
+            print("** class name doesn't exist **")
+
 
     def help_create(self):
         """ Help information for the create method """
-        print("Creates a class of any type")
+        print("Creates object of any type")
         print("[Usage]: create <className>\n")
 
     def do_show(self, args):
@@ -272,7 +283,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +291,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
