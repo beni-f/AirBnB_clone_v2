9808318d6073e6112c0ddaff06c8cffd696a287f
beni-f <benifissha@gmail.com>
2024-01-26 23:28:27 +0300

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-# A script that starts a Flask web app
+"""
+ A script that starts a Flask web app
+"""
 from flask import Flask
 
 app = Flask(__name__)
+app.strict_slashes =False
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
+    """
+        Return desired string
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
