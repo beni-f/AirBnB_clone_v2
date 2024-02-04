16e55de926bd5eb049792cf2929d968a16ba484b
beni-f <benifissha@gmail.com>
2024-02-04 12:17:16 +0300

    updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+    Starts a Flask web app
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,13 +10,16 @@ app = Flask(__name__)
 app.strict_slashes = False
 
 @app.teardown_appcontext
-def close(exception):
+def close():
     """
         Remove current session
     """
     storage.close()
 @app.route('/states_list')
 def state_list():
+    """
+        display a HTML page
+    """
     return render_template('7-states_list.html', states=storage.all(State).values())
 
 if __name__ == '__main__':
