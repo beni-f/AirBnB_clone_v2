17795ea796110ef9f46ff53c2a28112b2aaf37ea
beni-f <benifissha@gmail.com>
2024-02-04 12:15:05 +0300

    initial commit

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+@app.teardown_appcontext
+def close(exception):
+    """
+        Remove current session
+    """
+    storage.close()
+@app.route('/states_list')
+def state_list():
+    return render_template('7-states_list.html', states=storage.all(State).values())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port="5000", debug=True)
