2bea3d7eda2d3f0461ef5aef8b82fb3d5d98165b
beni-f <benifissha@gmail.com>
2024-02-04 12:32:52 +0300

    updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -23,4 +23,4 @@ def state_list():
     return render_template('7-states_list.html', states=storage.all(State).values())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port="5000", debug=True)
+    app.run(host='0.0.0.0', port="5000")
